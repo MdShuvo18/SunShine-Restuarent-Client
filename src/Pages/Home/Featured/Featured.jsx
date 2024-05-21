a99f9a50f3d2a48de1a2a5d7006c20d8f5f3ffa7
MdShuvo18 <mdks33881@gmail.com>
2024-05-21 09:24:56 +0600

    reviews section added

---
+++ src/Pages/Home/Featured/Featured.jsx
@@ -0,0 +1,30 @@
+import Sectiontitle from "../../../Components/SectionTitle";
+import './Featured.css'
+
+
+const Featured = () => {
+    return (
+        <div className="background-image bg-fixed text-white">
+            <Sectiontitle
+                subHeading={"check it out"}
+                heading={"from our menu"}
+            >
+
+            </Sectiontitle>
+
+            <div className="grid grid-cols-2 gap-4 justify-items-center py-20 px-16 mt-10 mb-10">
+                <div>
+                    <img className="w-[400px] h-[200px]" src="https://i.ibb.co/QnGY17K/farhad-ibrahimzade-Kp-Ol9j-V2a-JM-unsplash.jpg" alt="" />
+                </div>
+                <div className="space-y-2">
+                    <p>March 20, 2022</p>
+                    <h2>Where Can I Get Some</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis voluptatem, accusamus ut quidem facere aspernatur in nemo, qui eos possimus nulla ipsa sapiente perferendis iste vero corrupti praesentium reiciendis quasi?</p>
+                    <button className="btn btn-outline text-white">Read More</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,60 @@
+import Sectiontitle from "../../../Components/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
+
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+                console.log(data)
+            })
+    }, [])
+    return (
+        <div>
+            <section>
+                <Sectiontitle
+                    subHeading={"What Our Clients Say"}
+                    heading={"TESTIMONIALS"}
+                ></Sectiontitle>
+            </section>
+
+
+
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                {
+                    reviews.map(item => <SwiperSlide key={item._id}>
+
+                        <div className=" flex flex-col items-center space-y-2 text-center m-24">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={item.rating}
+                                readOnly
+
+                            />
+                            <p>{item.details}</p>
+                            <h2 className="text-2xl text-orange-400">{item.name}</h2>
+                        </div>
+
+                    </SwiperSlide>)
+                }
+
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Testimonials;
