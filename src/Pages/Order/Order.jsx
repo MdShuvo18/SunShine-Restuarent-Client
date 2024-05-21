52100cfe128321ff347b338b02dba29cca8e4d2e
MdShuvo18 <mdks33881@gmail.com>
2024-05-22 05:36:48 +0600

    order food cart added

---
+++ src/Pages/Order/Order.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Cover from "../Shared/Cover";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from "../../Hooks/useMenu";
+
+
+const Order = ({ img, title }) => {
+    const [tabIndex, setTabIndex] = useState(0)
+    const [menu] = useMenu()
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const salad = menu.filter(item => item.category === 'salad')
+    const soup = menu.filter(item => item.category === 'soup')
+    const offered = menu.filter(item => item.category === 'offered')
+    return (
+        <div>
+            <Cover img='https://i.ibb.co/dGqn3BG/micheile-henderson-1-Sj-D5-ZEi-Us-A-unsplash.jpg' title="Order Now" ></Cover>
+
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soups</Tab>
+                    <Tab>Desserts</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
+        </div>
+    );
+};
+
+export default Order;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,23 +1,28 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu";
+import Order from "../Pages/Order/Order";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children:[
-        {
-            path:'/',
-            element:<Home></Home>
-        },
-        {
-          path:'/menu',
-          element:<Menu></Menu>
-        }
-      ]
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/menu',
+        element: <Menu></Menu>
+      },
+      {
+        path: '/order',
+        element: <Order></Order>
+      }
+    ]
+  },
+]);
