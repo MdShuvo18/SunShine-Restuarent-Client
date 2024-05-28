35c110e19c49b7b9ecf86864d073e1ff3ba49ceb
MdShuvo18 <mdks33881@gmail.com>
2024-05-28 23:46:08 +0600

    signup form added using react hook form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,42 @@
+import { useContext, useEffect, useRef, useState } from 'react';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../Provider/AuthProvider/AuthProvider';
+import { Link } from 'react-router-dom';
 
 
 const Login = () => {
+    const captchaRef = useRef()
+    const [disabled, setDisabled] = useState(true)
+    const { user, signIn } = useContext(AuthContext)
 
-    const handleLogin=(e)=>{
+
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
+    const handleLogin = (e) => {
         e.preventDefault()
-        const form=e.target
-        const email=form.email.value;
-        const password=form.password.value
-        console.log(email,password)
+        const form = e.target
+        const email = form.email.value;
+        const password = form.password.value
+        // console.log(email, password)
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+
+    }
+
+    const handleCaptcha = () => {
+        const result = captchaRef.current.value;
+        if (validateCaptcha(result) == true) {
+            setDisabled(false)
+        }
+
+        else {
+            alert('Captcha Does Not Match');
+        }
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -32,10 +61,23 @@ const Login = () => {
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <LoadCanvasTemplate />
+                            </label>
+                            <input type="password" ref={captchaRef} name="captcha" placeholder="Type the captcha above" className="input input-bordered" required />
+
+                            <button onClick={handleCaptcha} className="btn btn-outline btn-xs mt-2">Validate</button>
+
+                        </div>
                         <div className="form-control mt-6">
-                            <input  className="btn btn-primary text-white" type="submit" value="Login" />
+                            <input disabled={disabled} className="btn btn-primary text-white" type="submit" value="Login" />
                         </div>
                     </form>
+
+                    <Link to='/signup'>
+                       <p className='text-lg font-semibold text-red-600 p-5'>New here!!! Create a new account</p>
+                    </Link>
                 </div>
             </div>
         </div>
